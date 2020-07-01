bff72e39f4b247824c9e670fd9d1500afc95765c
feiyangfan <feiyangfan1@gmail.com>
2020-07-01 17:53:29 -0400

    add clock function

--- webDesign/app.js
+++ webDesign/app.js
@@ -1,44 +1,45 @@
-function checkTime(i) {
-    if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
-    return i;
-}
-
-function startTime() {
-    var today = new Date();
-    var h = today.getHours();
-    var m = today.getMinutes();
-    var s = today.getSeconds();
-    m = checkTime(m);
-    s = checkTime(s);
-    document.getElementById('txt').innerHTML =
-    h + ":" + m + ":" + s;
-    var t = setTimeout(startTime, 500);
-}
-
-
-
-function changeActive() {
-    // Get the container element
-    var btnContainer = document.getElementById("sidenav-link");
-
-    // Get all buttons with class="btn" inside the container
-    var btns = btnContainer.getElementsByClassName("sidenav-link");
-
-    // Loop through the buttons and add the active class to the current/clicked button
-    for (var i = 0; i < btns.length; i++) {
-    btns[i].addEventListener("click", function() {
-        var current = document.getElementsByClassName("active");
-        current[0].className = current[0].className.replace(" active", "");
-        this.className += " active";
-    });
+function currentTime() {
+    var date = new Date(); /* creating object of Date class */
+    var hour = date.getHours();
+    var min = date.getMinutes();
+    var sec = date.getSeconds();
+    hour = updateTime(hour);
+    min = updateTime(min);
+    sec = updateTime(sec);
+    document.getElementById("clock").innerText = hour + " : " + min + " : " + sec; /* adding time to the div */
+    var t = setTimeout(function(){ currentTime() }, 1000); /* setting timer */
+  }
+  
+  function updateTime(k) {
+    if (k < 10) {
+      return "0" + k;
     }
-}
+    else {
+      return k;
+    }
+  }
+  
+  currentTime(); /* calling currentTime() function to initiate the process */
 
-function displayDate() {
 
-}
 
-function displayDate() {
+// function changeActive() {
+//     // Get the container element
+//     var btnContainer = document.getElementById("sidenav-link");
 
-}
+//     // Get all buttons with class="btn" inside the container
+//     var btns = btnContainer.getElementsByClassName("sidenav-link");
+
+//     // Loop through the buttons and add the active class to the current/clicked button
+//     for (var i = 0; i < btns.length; i++) {
+//     btns[i].addEventListener("click", function() {
+//         var current = document.getElementsByClassName("active");
+//         current[0].className = current[0].className.replace(" active", "");
+//         this.className += " active";
+//     });
+//     }
+// }
+
+
+
 
