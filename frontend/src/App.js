66b312bf64735be5dbb0b0c7b859068f71ef893c
Feiyang Fan <feiyangfan1@gmail.com>
2020-08-26 16:09:46 -0400

    update app.js and css

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Nav from "./components/Nav";
+import SubNav from "./components/SubNav";
 
 function App() {
+  //Write Javascript code here
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="navbar-div">
+        <Nav></Nav>
+      </div>
+      <div className="sub-nav">
+        <SubNav></SubNav>
+      </div>
+      <div className="main">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus
+        amet accusamus, nisi iure voluptatibus quasi illum libero doloremque
+        voluptates sint soluta perferendis maiores sit! Expedita unde placeat
+        nemo deleniti itaque.
+      </div>
     </div>
   );
 }
