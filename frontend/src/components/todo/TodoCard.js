5360db7e01c4a7df54593e09dbebae56b7100d6b
Feiyang Fan <feiyangfan1@gmail.com>
2020-08-31 21:17:44 -0400

    finished todo card system

--- frontend/src/components/todo/TodoCard.js
+++ frontend/src/components/todo/TodoCard.js
@@ -4,23 +4,29 @@ import "./TodoCard.css";
 import { ReactComponent as Plus } from "../../images/plus.svg";
 import TodoList from "./TodoList";
 
-const TodoCard = () => {
+const TodoCard = ({
+  cardName,
+  todoCards,
+  todoCard,
+  setListName,
+  setTodoCards,
+}) => {
   const [inputText, setInputText] = useState("");
   const [todos, setTodos] = useState([]);
-  const [listname, setListname] = useState("");
+
+  const deleteCardHandler = () => {
+    setTodoCards(todoCards.filter((element) => element.id !== todoCard.id));
+  };
 
   return (
     <div className="list-card">
       <div className="card-top">
-        <div className="card-name">List Name</div>
+        <div className="card-name">{cardName}</div>
         <div className="card-menu">
-          {/* <button className="card-add">
-            <Plus className="plus" />
-          </button> */}
-          <button className="card-edit" onClick={setListname}>
+          <button className="card-edit" onClick={setListName}>
             <Plus className="plus" />
           </button>
-          <button className="card-delete">
+          <button className="card-delete" onClick={deleteCardHandler}>
             <Plus className="plus" />
           </button>
         </div>
--- frontend/src/components/todo/TodoPage.js
+++ frontend/src/components/todo/TodoPage.js
@@ -2,15 +2,45 @@ import React, { useState } from "react";
 import TodoCard from "./TodoCard";
 import "./TodoPage.css";
 import SubNav from "../SubNav";
+import { v4 as uuidv4 } from "uuid";
 
+// !TODO
 const Todo = () => {
+  const [listName, setListName] = useState("");
   const [todoCards, setTodoCards] = useState([]);
+  const listNameHandler = (e) => {
+    console.log(e.target.value);
+    setListName(e.target.value);
+  };
+
+  const addCardHandler = (e) => {
+    e.preventDefault();
+    setTodoCards([...todoCards, { listName: listName, id: uuidv4() }]);
+    setListName("");
+  };
   return (
     <div className="todo-page">
       <div className="todo-nav">
-        <SubNav name="card" />
+        <SubNav
+          name="card"
+          addCardHandler={addCardHandler}
+          listName={listName}
+          todoCards={todoCards}
+          listNameHandler={listNameHandler}
+        />
       </div>
-      <TodoCard />
+      <ul className="todo-card-list">
+        {todoCards.map((todoCard) => (
+          <TodoCard
+            cardName={todoCard.listName}
+            key={todoCard.id}
+            todoCard={todoCard}
+            setTodoCards={setTodoCards}
+            todoCards={todoCards}
+            setListName={setListName}
+          />
+        ))}
+      </ul>
     </div>
   );
 };
