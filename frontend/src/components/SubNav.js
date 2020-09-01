a0e4c9abddf3ae276e41575a7a5a26793ab0df86
Feiyang Fan <feiyangfan1@gmail.com>
2020-08-31 21:17:31 -0400

    add subnav functionality

--- frontend/src/components/SubNav.js
+++ frontend/src/components/SubNav.js
@@ -1,15 +1,59 @@
-import React from "react";
+import React, { useState } from "react";
 import "./SubNav.css";
 import { ReactComponent as Plus } from "../images/plus.svg";
+import { Modal } from "@material-ui/core";
 
-const SubNav = ({ name }) => {
+const SubNav = ({
+  name,
+  addCardHandler,
+  listName,
+  listNameHandler,
+  todoCards,
+}) => {
+  const [open, setOpen] = useState(false);
+
+  const handleOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  const modalWindow = (
+    <div className="modal-window">
+      <div className="modal-window-name">Add new {name}</div>
+      <div className="modal-body">
+        <input
+          onChange={listNameHandler}
+          type="text"
+          className="modal-input"
+          placeholder="Enter your todo list name here"
+          value={listName}
+        ></input>
+        <button
+          className="modal-button"
+          type="submit"
+          onClick={
+            todoCards.length < 3 ? (listNameHandler, addCardHandler) : ""
+          }
+        >
+          <Plus className="plus" />
+        </button>
+      </div>
+    </div>
+  );
+
   return (
     <div className="subnav">
       <div className="subnav-right">
-        <button className="subnav-add-button">
+        <button className="subnav-add-button" onClick={handleOpen}>
           <Plus className="plus"></Plus>
           <span className="button-text">Add {name}</span>
         </button>
+        <Modal className="modal" open={open} onClose={handleClose}>
+          {modalWindow}
+        </Modal>
       </div>
     </div>
   );
