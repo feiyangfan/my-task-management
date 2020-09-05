66384ba7a11278c3e14810a890c63058a926492c
Feiyang Fan <feiyangfan1@gmail.com>
2020-09-05 18:47:04 -0400

    install dotenv and save api key in .env

--- backend/routes/weatherAPI.js
+++ backend/routes/weatherAPI.js
@@ -1,13 +1,15 @@
 var express = require("express");
 var request = require("request");
 var router = express.Router();
+const envFile = require("dotenv").config();
+const WEATHER_API_KEY = process.env.WEATHER_API_KEY;
 
 /* GET users listing. */
 router.post("/", (req, res) => {
   console.log(req.body.query);
   const city = req.body.query;
   request(
-    `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&APPID=ec9bb1eaae55cb44b0ecbaae5c4a7280`,
+    `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&APPID=${WEATHER_API_KEY}`,
     function (error, response, body) {
       if (!error && response.statusCode == 200) {
         const parsedBody = JSON.parse(body);
