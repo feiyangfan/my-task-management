17017106a77d689cb017780a9818cc02a207299d
Feiyang Fan <feiyangfan1@gmail.com>
2020-09-18 20:09:50 -0400

    add Todo model

---
+++ backend/models/Todo.js
@@ -0,0 +1,22 @@
+const mongoose = require("mongoose");
+
+const TodoSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  body: {
+    type: String,
+    required: true,
+  },
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = mongoose.model("Todo", TodoSchema);
