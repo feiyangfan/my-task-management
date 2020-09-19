414ed4ce1074844c4f4c2ef3cb2cd718cb0b5888
Feiyang Fan <feiyangfan1@gmail.com>
2020-09-18 20:14:24 -0400

    change weather route

--- backend/routes/weather.js
+++ backend/routes/weather.js
@@ -4,16 +4,6 @@ const router = express.Router();
 require("dotenv").config({ path: "./config/config.env" });
 const WEATHER_API_KEY = process.env.WEATHER_API_KEY;
 
-const getRequest = async (city) => {
-  console.log(city);
-  let res = await axios.get(
-    `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&APPID=${WEATHER_API_KEY}`
-  );
-
-  let data = res.data;
-  return data;
-};
-
 router.post("/", (req, res) => {
   console.log(req.body);
   const city = req.body.city;
@@ -26,18 +16,6 @@ router.post("/", (req, res) => {
       console.log(resp.data);
       res.send(resp.data);
     });
-  // axios.get(
-  //   `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&APPID=${WEATHER_API_KEY}`,
-  //   function (error, response, body) {
-  //     console.log(response);
-  //     console.log(body);
-  //     if (!error && response.statusCode == 200) {
-  //       const parsedBody = JSON.parse(body);
-  //       console.log(parsedBody);
-  //       res.send(parsedBody);
-  //     }
-  //   }
-  // );
 });
 
 module.exports = router;
