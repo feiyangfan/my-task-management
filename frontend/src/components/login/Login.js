0b8d1001f3192ae0cb7b9508c6e67850cd182a99
Feiyang Fan <feiyangfan1@gmail.com>
2020-09-19 23:05:09 -0400

    finished landing page

--- frontend/src/components/login/Login.js
+++ frontend/src/components/login/Login.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { Button, Card } from "@material-ui/core";
 import "./Login.css";
-const { createProxyMiddleware } = require("http-proxy-middleware");
 
 const Login = () => {
   return (
@@ -11,7 +10,7 @@ const Login = () => {
           <h1>My Daily Planner</h1>
           <p>Weather, Todo Lists, Timetable, and more!</p>
         </div>
-        <Button variant="outlined" color="secondary">
+        <Button variant="outlined" color="secondary" className="login-btn">
           <a href="http://localhost:9000/auth/google">
             Login with Google account
           </a>
