3f99ca2525d3a6de697a3828740054e65e5215f7
Feiyang Fan <feiyangfan1@gmail.com>
2020-09-19 23:04:54 -0400

    finished dashboard showing logged in user name

--- frontend/src/components/dashboard/Card.js
+++ frontend/src/components/dashboard/Card.js
@@ -2,8 +2,6 @@ import React from "react";
 import "./Card.css";
 
 const Card = (props) => {
-  console.log("props is ===>");
-  console.log(props);
   return (
     <div className="card">
       <props.content></props.content>
--- frontend/src/components/dashboard/Dashboard.js
+++ frontend/src/components/dashboard/Dashboard.js
@@ -1,17 +1,38 @@
 import React from "react";
+import { useEffect, useState } from "react";
 import Weather from "./Weather";
 import "./Dashboard.css";
 import Card from "./Card";
-import SubNav from "../SubNav";
 
 const Dashboard = () => {
+  const [user, setUser] = useState("");
+
+  const getUser = async () => {
+    const request = await fetch("http://localhost:9000/auth/login/success", {
+      method: "GET",
+      credentials: "include",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+        "Access-Control-Allow-Credentials": true,
+      },
+    });
+    const result = await request.json();
+    // console.log(result.user);
+    setUser(result.user.displayName);
+  };
+  useEffect(() => {
+    getUser();
+  }, []);
+
   return (
     <div className="dashboard">
-      <div className="dashboard-subnav">
+      {/* <div className="dashboard-subnav">
         <SubNav name="dashboard" />
-      </div>
+      </div> */}
 
       <div className="dashboard-main-display">
+        <h2>Welcome, {user}</h2>
         <Card content={Weather} />
       </div>
     </div>
